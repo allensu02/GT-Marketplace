bddd35ebef73dcc9ce4a6726eabbdb0947ee0032
Allen Su <allenysu@gmail.com>
2022-09-27 17:42:25 -0400

    Created project

---
+++ GT Marketplace/GT_MarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GT_MarketplaceApp.swift
+//  GT Marketplace
+//
+//  Created by Allen Su on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct GT_MarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
