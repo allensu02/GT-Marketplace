6b0e3cbef07b80b1cd8773a33461190dbb770710
ezhou66 <ericzhou3@gmail.com>
2022-10-04 17:29:45 -0400

    testpage

---
+++ GT Marketplace/ListingView.swift
@@ -0,0 +1,22 @@
+//
+//  ListingView.swift
+//  GT Marketplace
+//
+//  Created by Eric Zhou on 9/27/22.
+//
+
+import SwiftUI
+
+struct ListingView: View {
+    var body: some View {
+        VStack {
+            Text("Listings")
+        }
+    }
+}
+
+struct ListingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
