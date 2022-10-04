a5eb091ea34a08018c45692d4809eecfcf131e3d
Rishi Aniga <rishianiga@lawn-128-61-2-131.lawn.gatech.edu>
2022-10-04 18:58:01 -0400

    updated slideshow feature

--- GT Marketplace/SlideshowView.swift
+++ GT Marketplace/SlideshowView.swift
@@ -9,15 +9,64 @@ import SwiftUI
 
 struct SwiftUIView: View {
     private var numberOfImages = 4
-    var body: some View {
-        TabView {
-            ForEach(0..<numberOfImages, id: \.self){ num in Image("\(num)")
-                    .resizable()
-                    .scaledToFit()
-                    .tag(num)
-                
+    private  let timer = Timer.publish(every: 3, on:
+            .main, in: .common).autoconnect()
+    @State private var currentIndex = 0;
+    
+    func previous() {
+        withAnimation {
+            currentIndex = currentIndex > 0 ? currentIndex
+            - 1 : numberOfImages - 1
+        }
+    }
+    
+    func next() {
+        withAnimation {
+            currentIndex = currentIndex <
+                numberOfImages ? currentIndex + 1 :
+            0
+        }
+    }
+    
+    var controls: some View {
+        HStack {
+            Button {
+                previous()
+            } label: {
+                Image(systemName: "chevron.left")
             }
-        }.tabViewStyle(PageTabViewStyle())
+            
+            Button {
+                next()
+            } label: {
+                Image(systemName: "chevron.left")
+            }
+        }
+    }
+    
+    var body: some View {
+        GeometryReader { proxy in
+            VStack {
+                TabView(selection: $currentIndex) {
+                    ForEach(1..<numberOfImages + 1, id: \.self){ num in
+                        Image("\(num)")
+                            .resizable()
+                            .scaledToFit()
+                            .overlay(Color.black.opacity(0.4))
+                            .tag(num)
+                        
+                    }
+                }.tabViewStyle(PageTabViewStyle())
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                    .padding()
+                    .frame(width: proxy.size.width, height:
+                            proxy.size.height / 3)
+                    .onReceive(timer, perform: { _ in
+                        next()
+                    })
+                controls
+            }
+        }
     }
 }
 
