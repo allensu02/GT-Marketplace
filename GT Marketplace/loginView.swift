e02bbd9995622c64de49af5afc29b0369aec7b7f
Yoon Ji Cho <yoonjicho@Yoons-MacBook-Pro-3.local>
2022-10-27 18:47:36 -0400

    created loginView

---
+++ GT Marketplace/loginView.swift
@@ -0,0 +1,20 @@
+//
+//  loginView.swift
+//  GT Marketplace
+//
+//  Created by Yoon Ji Cho on 10/25/22.
+//
+
+import SwiftUI
+
+struct loginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct loginView_Previews: PreviewProvider {
+    static var previews: some View {
+        loginView()
+    }
+}
