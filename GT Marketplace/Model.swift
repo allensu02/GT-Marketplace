848e246bc20ad562b6a82021154eacb0e592492c
Yuan, Max <myuan41@gatech.edu>
2022-11-03 18:45:04 -0400

    Connected to firebase

--- GT Marketplace/Model.swift
+++ GT Marketplace/Model.swift
@@ -9,20 +9,56 @@ import Foundation
 import FirebaseDatabase
 import FirebaseDatabaseSwift
 
-class Model: ObservableObject {
-  private let ref = Database.database().reference()
-
-  
-  func pushListing(title: String, price: Int) {
-    var newListing = Listing()
-    newListing.id = UUID()
-    newListing.title = title
-    newListing.price = price
-    ref.child(newListing.id.uuidString).setValue(newListing.toDictionary)
-  }
-  
-  func deleteListing(title: String) {
+final class Model: ObservableObject {
+    @Published var listings: [Listing] = []
     
-  }
+    private let ref = Database.database().reference()
+    
+    private lazy var databasePath: DatabaseReference? = {
+        let ref = Database.database().reference().child("Listings")
+        return ref
+    }()
+    
+    private let encoder = JSONEncoder()
+    private let decoder = JSONDecoder()
+    
+    func listentoRealtimeDatabase() {
+        guard let databasePath = databasePath else {
+            return
+        }
+        databasePath
+            .observe(.childAdded) { [weak self] snapshot in
+                guard
+                    let self = self,
+                    var json = snapshot.value as? [String: Any]
+                else {
+                    return
+                }
+                json["id"] = snapshot.key
+                do {
+                    let ListingData = try JSONSerialization.data(withJSONObject: json)
+                    let listing = try self.decoder.decode(Listing.self, from: ListingData)
+                    self.listings.append(listing)
+                } catch {
+                    print("an error occurred", error)
+                }
+            }
+    }
+    
+    func pushListing(title: String, price: Int) {
+      var newListing = Listing()
+      newListing.id = UUID()
+      newListing.title = title
+      newListing.price = price
+      ref.child("Listings/" + newListing.id.uuidString).setValue(newListing.toDictionary)
+    }
+    
+    func deleteListing(id: String) {
+        ref.child("Listings/" + id).removeValue()
+    }
+    
+    func stopListening() {
+        databasePath?.removeAllObservers()
+    }
 }
 
