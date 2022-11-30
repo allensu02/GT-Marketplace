011a621d87ab1093929a45992f631a1462e52153
Shreya Malpani <smalpani8@gatech.edu>
2022-11-29 19:09:50 -0500

    working login

---
+++ GT Marketplace/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  GT Marketplace
+//
+//  Created by Shreya Malpani on 11/8/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
