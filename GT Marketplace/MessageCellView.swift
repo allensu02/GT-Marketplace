8e9733a462dd4392c2e7b0f28347f2ed79e713e2
Mark Lau <mlau41@gatech.edu>
2022-12-01 19:13:29 -0500

    implemented search bar and message list view

---
+++ GT Marketplace/MessageCellView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageCellView.swift
+//  GT Marketplace
+//
+//  Created by Mark Lau on 11/15/22.
+//
+
+import SwiftUI
+
+struct MessageCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageCellView()
+    }
+}
---
+++ GT Marketplace/MessagesListView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesListView.swift
+//  GT Marketplace
+//
+//  Created by Mark Lau on 11/15/22.
+//
+
+import SwiftUI
+
+struct MessagesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessagesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesListView()
+    }
+}
